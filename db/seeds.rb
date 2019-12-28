c3ea10ea508cd4202164c9cba689988d90fb0b92
Ernst Rullmann <e.rullmann@gmail.com>
2019-12-27 19:32:41 -0800

    Updated seeds to have better markdown content.

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,10 +2,20 @@ admin = Admin.create(email: 'testadmin@rullmann.ca', password: 'Testing123', pas
 
 emoji = %w[🤠 😁 👨‍💻 🖖 👋 🧵 👑 🙏]
 
-(1..20).each do |index|
-  Article.create(skip_date_validation: true, admin: admin, title: Faker::Lorem.sentence, body: Faker::Markdown.sandwich(sentences: (5 * rand).to_i), emoji: emoji.sample, location: Faker::Nation.capital_city, publish_date: (index.days.ago))
+# have to do this because Faker has bug with inserting tables
+def makdown_sandwich(sentences: 3, repeat: 1)
+  text_block = []
+  repeat.times do
+    text_block << Faker::Lorem.sentences(number: sentences)
+    text_block << "\n" + Faker::Markdown.random
+  end
+  text_block.join("\n")
+end
+
+(1..50).each do |index|
+  Article.create(skip_date_validation: true, admin: admin, title: Faker::Lorem.sentence, body: makdown_sandwich(sentences: (5 * rand + 1).to_i, repeat: (3 * rand + 1).to_i), emoji: emoji.sample, location: Faker::Nation.capital_city, publish_date: (index.days.ago))
 end
 
 (1..5).each do |index|
-  Article.create(admin: admin, title: Faker::Lorem.sentence, body: Faker::Markdown.sandwich(sentences: (5 * rand).to_i), emoji: emoji.sample, location: Faker::Nation.capital_city, publish_date: (index.days.from_now))
+  Article.create(admin: admin, title: Faker::Lorem.sentence, body: makdown_sandwich(sentences: (5 * rand + 1).to_i, repeat: (3 * rand + 1).to_i), emoji: emoji.sample, location: Faker::Nation.capital_city, publish_date: (index.days.from_now))
 end
