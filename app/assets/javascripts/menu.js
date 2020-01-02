cc195934d6b9c1880a1a0e3af26b83784b1d341c
Ernst Rullmann <e.rullmann@gmail.com>
2020-01-02 14:35:40 -0800

    Fixed turbolinks toggling issue with menu.

--- app/assets/javascripts/menu.js
+++ app/assets/javascripts/menu.js
@@ -1,6 +1,7 @@
 // from https://bulma.io/documentation/components/navbar/
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('turbolinks:load', () => {
+    console.log("DOM loaded")
 
     // Get all "navbar-burger" elements
     const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
@@ -11,6 +12,7 @@ document.addEventListener('DOMContentLoaded', () => {
         // Add a click event on each of them
         $navbarBurgers.forEach( el => {
             el.addEventListener('click', () => {
+                console.log("toggling")
 
                 // Get the target from the "data-target" attribute
                 const target = el.dataset.target;
