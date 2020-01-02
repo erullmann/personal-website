19a6055545e3ccf5861a9ab355fca7fe32a91749
Ernst Rullmann <e.rullmann@gmail.com>
2020-01-02 13:29:46 -0800

    Added extra logging for feed job.

--- app/jobs/feed_job.rb
+++ app/jobs/feed_job.rb
@@ -1,6 +1,10 @@
 class FeedJob < ApplicationJob
   def perform
+    puts "Performing feed job"
+
     Feed.where("last_fetched_at IS NULL OR last_fetched_at + (minutes_between_fetches ||' minutes')::interval < current_timestamp").all.find_each do |feed|
+      puts "Retrieving feed for #{feed.name}"
+
       feed.retrieve_feed
     end
   end
