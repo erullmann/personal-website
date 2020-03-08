5f741dc7e119b7911442981cb296941096f977c6
Ernst Rullmann <e.rullmann@gmail.com>
2020-03-08 15:56:25 -0400

    Cleaned up feed fetcher script.

--- personal-website/feed_fetcher.sh
+++ personal-website/feed_fetcher.sh
@@ -2,8 +2,4 @@
 
 date > /tmp/date
 
-source $SECRETS_FILE
-
-cd /usr/src/app
-
-RAILS_ENV=production bundle exec rails feed_runner:fetch_feed > /tmp/feed_output 2> /tmp/feed_error
+bundle exec rails feed_runner:fetch_feed
