4057c611b517c11a64638072b6a5ba4f69073a89
Ernst Rullmann <e.rullmann@gmail.com>
2020-03-08 14:25:48 -0400

    Enabled SSL and prod deployment for website.

--- nginx-proxy/renew_ssl.sh
+++ nginx-proxy/renew_ssl.sh
@@ -1,15 +1,3 @@
 #! /bin/bash
 
-sed -i -r 's/(listen .*443)/\1;#/g; s/(ssl_(certificate|certificate_key|trusted_certificate) )/#;#\1/g' /etc/nginx/sites-enabled/erullmann.ca.conf
-
-nginx -t && systemctl reload nginx
-
-certbot certonly --webroot -d erullmann.ca -d www.erullmann.ca --email e.rullmann@gmail.com -w /storage/_letsencrypt -n --agree-tos --force-renewal
-
-sed -i -r 's/#?;#//g' /etc/nginx/sites-enabled/erullmann.ca.conf
-
-nginx -t && systemctl reload nginx
-
-echo -e '#!/bin/bash\nnginx -t && systemctl reload nginx' | tee /etc/letsencrypt/renewal-hooks/post/nginx-reload.sh
-
-chmod a+x /etc/letsencrypt/renewal-hooks/post/nginx-reload.sh
+certbot renew
--- personal-website/docker_run.sh
+++ personal-website/docker_run.sh
@@ -1,5 +1,12 @@
 #! /bin/bash
 
 bundle exec rails assets:precompile
+
+mount | grep -q /assets
+if [[ $? = 0 ]]; then
+  echo "Copying assets..."
+  rsync -ak --delete public/ /assets
+fi
+
 bundle exec rails db:migrate
 bundle exec puma -C config/puma.rb
